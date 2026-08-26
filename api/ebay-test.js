ced4f734512312d42308a3a947e437dbcad44cc8
Barakahtags <ssiuk@live.co.uk>
2026-08-26 04:04:57 +0200

    Remove redundant eBay test endpoint to reduce function count

--- api/ebay-test.js
+++
@@ -1,5 +0,0 @@
-const ebay=require('./_lib/ebay');
-module.exports=async function(req,res){
-  try{const privileges=await ebay.api('/sell/account/v1/privilege/');res.status(200).json({ok:true,message:'Production eBay OAuth connection is working.',refreshReady:!!(process.env.EBAY_CLIENT_SECRET&&process.env.EBAY_REFRESH_TOKEN),privileges});}
-  catch(e){res.status(e.status||500).json({ok:false,error:e.message,details:e.data||null,refreshReady:!!(process.env.EBAY_CLIENT_SECRET&&process.env.EBAY_REFRESH_TOKEN)});}
-};
